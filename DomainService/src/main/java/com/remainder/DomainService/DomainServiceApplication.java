c14f6e47966e822c4b876f6132e6788342e7cb4d
Lokesh Reddy Karakala <lokesh.k@comakeit.com>
2022-11-17 09:50:10 +0530

    application profiles added

--- DomainService/src/main/java/com/remainder/DomainService/DomainServiceApplication.java
+++ DomainService/src/main/java/com/remainder/DomainService/DomainServiceApplication.java
@@ -1,5 +1,7 @@
 package com.remainder.DomainService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class DomainServiceApplication {
 
+	public static final Logger logger = LogManager.getLogger(DomainServiceApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(DomainServiceApplication.class, args);
+		System.out.println("Hello World");
+		logger.info("Info level log message");
 	}
 
 }
